0b6ea4c9904292fa6e758a819e89060fe5de255c
tnanhekhan <1631995@gmail.com>
2020-05-25 10:24:36 +0200

    fix gulp travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+const gulp = require('gulp');
+const replace = require('gulp-replace')
+
 function defaultTask(cb) {
-    gulp.task('templates', () => {
-        gulp.src()
-    })
+    gulp.src("functions/index.js")
+        .pipe(replace("admin.initializeApp({credential: credential, databaseURL: databaseURL});", "admin.initializeApp();"))
+        .pipe(gulp.dest('functions/'));
+
     cb();
 }
 
