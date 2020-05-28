e3c28620000256ab17bac74ed413fafb2e504eca
tnanhekhan <1631995@gmail.com>
2020-05-28 16:26:10 +0200

    able to add words per word list

--- functions/routes/cms.js
+++ functions/routes/cms.js
@@ -12,11 +12,7 @@ router.get("/", (req, res, next) => {
 router.get('/word-lists', (req, res, next) => {
     db.collection('wordlists').get()
         .then(snapshot => {
-            let wordlists = []
-            snapshot.forEach(doc => {
-                wordlists.push(doc)
-            });
-            return wordlists
+            return snapshot.docs;
         })
         .then(wordLists => {
             res.render("wordlists/wordlists-get", {title: "CMS", dest: "wordlists", wordLists: wordLists});
@@ -26,15 +22,8 @@ router.get('/word-lists', (req, res, next) => {
         });
 });
 
-router.post("/word-lists", (req, res, next) => {
-    const word = req.body.word;
-    wordCollection.add({word: word, timestamp: Date.now()}).then(ref => {
-        res.render("wordlists/wordlists-post", {title: "CMS", uploadedWord: word});
-    })
-});
-
 router.get('/word-lists/add', (req, res, next) => {
-    res.render("wordlists/add", {title: "CMS", dest: "wordlists"});
+    res.render("wordlists/add-wordlist", {title: "CMS", dest: "wordlists"});
 });
 
 router.get('/word-lists/:listId', (req, res, next) => {
@@ -46,9 +35,72 @@ router.get('/word-lists/:listId', (req, res, next) => {
                     return snapshot.docs;
                 })
                 .then(words => {
-                    res.render("wordlists/words", {title: "CMS", dest: "wordlists", words: words, listId: req.params.listId});
+                    res.render("wordlists/words", {
+                        title: "CMS",
+                        dest: "wordlists",
+                        words: words,
+                        listId: req.params.listId
+                    });
                 })
         });
+});
+
+router.get("/word-lists/:listId/add", (req, res, next) => {
+    res.render("wordlists/word-detail", {
+        title: "CMS",
+        dest: "wordlists",
+        word: "",
+        listId: req.params.listId,
+    });
+});
+
+router.post("/word-lists/:listId/add", (req, res, next) => {
+    if ("save-word" in req.body && req.body.word) {
+        db.collection("wordlists").doc(req.params.listId).listCollections()
+            .then(collections => {
+                const collectionId = collections[0].id;
+                db.collection("wordlists").doc(req.params.listId).collection(collectionId)
+                    .add({word: req.body.word, timestamp: Date.now()})
+                    .then(() => {
+                        res.redirect(`/cms/word-lists/${req.params.listId}`);
+                    });
+            });
+    } else {
+        res.redirect(`/cms/word-lists/${req.params.listId}`);
+    }
+});
+
+router.get("/word-lists/:listId/:wordId", (req, res, next) => {
+    db.collection("wordlists").doc(req.params.listId).listCollections()
+        .then(collections => {
+            const collectionId = collections[0].id;
+            db.collection("wordlists").doc(req.params.listId).collection(collectionId).doc(req.params.wordId).get()
+                .then(result => {
+                    res.render("wordlists/word-detail", {
+                        title: "CMS",
+                        dest: "wordlists",
+                        word: result.data().word,
+                        listId: req.params.listId,
+                        wordId: req.params.wordId,
+                    });
+                });
+        });
+});
+
+router.post("/word-lists/:listId/:wordId", (req, res, next) => {
+    if ("save-word" in req.body && req.body.word) {
+        db.collection("wordlists").doc(req.params.listId).listCollections()
+            .then(collections => {
+                const collectionId = collections[0].id;
+                db.collection("wordlists").doc(req.params.listId).collection(collectionId).doc(req.params.wordId)
+                    .update({word: req.body.word, timestamp: Date.now()})
+                    .then(() => {
+                        res.redirect(`/cms/word-lists/${req.params.listId}`);
+                    });
+            });
+    } else {
+        res.redirect(`/cms/word-lists/${req.params.listId}`);
+    }
 });
 
 module.exports = router;
