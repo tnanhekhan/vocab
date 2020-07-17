fdba491835edaefccda2cda90346731adcf5e6b3
heralt <heralt@live.nl>
2020-07-17 15:20:13 +0200

    cleanup dialogflow code

--- functions/dialogflow/dialogflow-app.js
+++ functions/dialogflow/dialogflow-app.js
@@ -11,19 +11,6 @@ app.intent('Welcome', conv => {
     intent.welcome(conv);
 });
 
-app.intent('Begin', conv => {
-    return intent.begin(conv)
-        .then(result => {
-            conv.ask(result.woord);
-            conv.ask(new BasicCard({
-                title: result.woord,
-                image: new Image({
-                    url: result.plaatje
-                }),
-            }));
-    });
-});
-
 app.intent('Woordjes', (conv, {gesprokenWoord}) => {
     return intent.woorden(conv,{gesprokenWoord}).then(result => {
         conv.ask(result.woord);
@@ -34,6 +21,30 @@ app.intent('Woordjes', (conv, {gesprokenWoord}) => {
             })
         }));
     });
+});
+
+app.intent('spreuk', conv => {
+    conv.ask('Leuk dat je komt oefenen. ' +
+        'Wat is de geheime spreuk die je van je meester of juf hebt gekregen?' +
+        ' Als je geen spreuk hebt, zeg dan ik heb geen spreuk');
+});
+
+app.intent('lijst spreuk', (conv, {spreuk}) => {
+    return intent.lijstSpreuk(conv, {spreuk}).then( result => {
+        conv.ask(`Hallo ${result.naam}. Zeg start om te beginnen`);
+    });
+});
+
+app.intent('start', conv => {
+    return intent.start(conv).then(result => {
+        conv.ask(result.woord);
+        conv.ask(new BasicCard({
+            title: result.woord,
+            image: new Image({
+                url: result.plaatje
+            }),
+        }));
+    }).catch(e => {console.error('error ',e)});
 });
 
 app.intent('Fallback', conv => {
--- functions/dialogflow/intents-logic.js
+++ functions/dialogflow/intents-logic.js
@@ -1,94 +1,58 @@
-const {
-    dialogflow
-} = require("actions-on-google");
-const fbConfig = require("../firebase");
-const db = fbConfig.firestore();
-const bucket = fbConfig.storage().bucket();
-
-let incorrect_guesses = 0;
-const moeilijkeWoorden = [];
-
-async function fetchWoorden(){
-    let data = [];
-    let getLijst = await db.collection('students').doc('8Bnz4LbO0ywimt0oGjY5').get();
-    let wordCollection = db.collection('wordlists').doc(getLijst.data().woordenlijst).collection("wordCollection");
-    let snapshot = await wordCollection.get();
-    snapshot.forEach(woord => {
-        data.push(woord);
-    });
-    let woorden = data.map(woord => {
-        return {
-            id: woord.id,
-            woord: woord.data().word
-        }
-    });
-    return woorden;
-}
-
-async function fetchImages(imageID) {
-    let result = await bucket.file(imageID).get();
-    return result[1].mediaLink;
-}
-
-async function sendProgress(completedWords, difficultWords, woordenlijst) {
-    const studentProgressie = db.collection('progression').doc('J0Ijqla8aZG6HC9CRIYp');
-    let geoefend = await studentProgressie.get();
-    geoefend = geoefend.data().woordenGeoefend + completedWords;
-    console.log('aantalwoorden ',geoefend);
-        studentProgressie.get().update({
-            woordenGeoefend: geoefend,
-            student: '8Bnz4LbO0ywimt0oGjY5',
-            moeilijkeWoorden: difficultWords,
-            woordenlijst: woordenlijst
-        });
-}
+const data = require('./fetchData');
 
 exports.welcome = conv => {
-    if (!conv.surface.capabilities.has('actions.capability.INTERACTIVE_CANVAS')) {
+    if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
         conv.close('sorrie, dit apparaat understeund deze app niet!');
         return;
     }
-    conv.ask('hi, welkom in de vocab app, ben je klaar?');
     conv.data.index = 0;
+    conv.data.woordenlijst = [];
+    conv.ask('hi, welkom in de vocab app? Met mij kun je woordjes oefenen. Als je uitleg wilt, zeg dan / ik wil uitleg.\n' +
+        'Als je wilt oefenen zeg dan /ik wil oefenen');
 };
 
-exports.begin = conv => {
-    conv.data.woordenlijst = [];
-    return fetchWoorden().then(woorden => {
-        conv.data.woordenlijst = woorden;
-        return fetchImages(conv.data.woordenlijst[conv.data.index].id).then(result => {
-            return {woord: conv.data.woordenlijst[conv.data.index].woord, plaatje: result, event: 'OEFENEN'};
+exports.lijstSpreuk = (conv, {spreuk}) => {
+    return data.fetchList(spreuk).then(result => {
+        return data.fetchName(result[0].student).then(name => {
+            return data.fetchWoorden(result[0].list).then(woorden => {
+                conv.data.woordenlijst = woorden;
+                return {naam: name};
+            });
         });
+    });
+};
+
+exports.start = (conv) => {
+    return data.fetchImages(conv.data.woordenlijst[conv.data.index].id).then(result => {
+        return {woord: conv.data.woordenlijst[conv.data.index].woord, plaatje: result};
     });
 };
 
 exports.woorden = (conv, {gesprokenWoord}) => {
+    const moeilijkeWoorden = [];
     const aantalWoorden = conv.data.woordenlijst.length - 1;
     let laatsteWoord = conv.data.woordenlijst[conv.data.woordenlijst.length - 1].woord.toLowerCase();
-    let value = '';
+    conv.data.incorrect_guesses = 0;
 
     if (gesprokenWoord === laatsteWoord) {
-        sendProgress(aantalWoorden, moeilijkeWoorden, 'list two');
-        return value = new Promise((resolve, reject) => {
-            resolve({woord: 'goed gedaan! tot de volgende keer!', event: 'KLAAR', plaatje: null});
-        });
+        data.sendProgress(aantalWoorden, moeilijkeWoorden, 'list two');
     } else {
-        if ((gesprokenWoord.toLowerCase() !== conv.data.woordenlijst[conv.data.index].woord.toLowerCase()) && (incorrect_guesses < 1)) {
-            incorrect_guesses += 1;
-            conv.data.woord = {word: conv.data.woordenlijst[conv.data.index].woord, tries: incorrect_guesses};
+        if ((gesprokenWoord.toLowerCase() !== conv.data.woordenlijst[conv.data.index].woord.toLowerCase()) && (conv.data.incorrect_guesses < 1)) {
+            conv.data.incorrect_guesses += 1;
+            conv.data.woord = {word: conv.data.woordenlijst[conv.data.index], tries: conv.data.incorrect_guesses};
             conv.ask('Helaas, dat is niet goed! Laten we het nog een keer proberen!')
         } else if (conv.data.index !== aantalWoorden) {
             if (conv.data.woord !== "") {
                 moeilijkeWoorden.push(conv.data.woord);
             }
             conv.data.woord = "";
-            incorrect_guesses = 0;
+            conv.data.incorrect_guesses = 0;
             conv.data.index += 1;
-            conv.ask('Dat is goed!');
+            conv.ask('Goedzo, volgende vraag!');
         }
-        return fetchImages(conv.data.woordenlijst[conv.data.index].id)
+        return data.fetchImages(conv.data.woordenlijst[conv.data.index].id)
             .then(result => {
-                return {woord: conv.data.woordenlijst[conv.data.index].woord, plaatje: result, event: 'OEFENEN'};
+                return {woord: conv.data.woordenlijst[conv.data.index].woord, plaatje: result};
             });
     }
 };
